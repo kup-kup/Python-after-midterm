7621c423505170ca864b73deeb91096e4d4878b9
kup <konka.um@gmail.com>
2023-10-19 23:19:52 +0700

    everything done

--- lab1-2/main.py
+++ lab1-2/main.py
@@ -48,3 +48,82 @@ Address:    59/1 Charat Mueng Road,
 # print(c)
 
 #8
+
+# str = "Hello World!"
+
+# print(str)
+# print(str[0])
+# print(str[2:5])
+# print(str[2:])
+# print(str * 2)
+# print(str + "TEST")
+
+#9-a
+
+# list = ["abcd", 786, 2.23, "john", 70.2]
+# tinylist = [123, "john"]
+
+# print(list)
+# print(list[0])
+# print(list[1:3])
+# print(list[2:])
+# print(tinylist * 2)
+# print(list + tinylist)
+
+#9-b
+
+# l1 = [1, 2, 3]
+# l2 = ['a', 'b', 'c']
+# print(l1 + l2)
+
+#10
+
+# tuple = ("abcd", 786, 2.23, "john", 70.2)
+# tinytuple = (123, "john")
+
+# print(tuple)
+# print(tuple[0])
+# print(tuple[1:3])
+# print(tuple[2:])
+# print(tinytuple * 2)
+# print(tuple + tinytuple)
+
+#11
+
+# tuple = ("abcd", 786, 2.23, "john", 70.2)
+# lsit = ["abcd", 786, 2.23, "john", 70.2]
+# tuple[2] = 1000
+# list[2] = 1000
+
+#12
+
+# dict = {}
+# dict["one"] = "This is one"
+# dict[2] = "This is two"
+
+# tinydict = {"name": "john", "code": 6734, "dept": "sales"}
+
+# print(dict["one"])
+# print(dict[2])
+# print(tinydict)
+# print(tinydict.keys())
+# print(tinydict.values())
+
+personal_info = {
+    "first_name": "Konkanok",
+    "last_name": "Umnartyuttithum",
+    "student_number": "6642003026",
+    "age": {
+        "year": 18, 
+        "month": 5,
+        "day": 20
+    },
+    "address": {
+        "h_num": "59/1",
+        "street": "Charat Mueng",
+        "sub-district": "Rong Mueng",
+        "district": "Pathumwan",
+        "province": "Bangkok",
+        "postal": "10330"
+    }
+}
