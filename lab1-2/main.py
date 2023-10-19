ebeb8639618d680ae676999ae9cfb8235efd6b5d
kup <konka.um@gmail.com>
2023-10-19 20:54:31 +0700

    done to 5-b

--- lab1-2/main.py
+++ lab1-2/main.py
@@ -11,4 +11,12 @@ Address:    59/1 Charat Mueng Road,
 
 #3
 
-print("Hello, Konkanok Umnartyuttithum, 6642003026")
+#print("Hello, Konkanok Umnartyuttithum, 6642003026")
+
+#5-a
+
+# help(print)
+
+#5-b
+
+print(1, 2, 3, 4, sep = ":", end = "-")
