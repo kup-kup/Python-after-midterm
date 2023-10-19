3c5c1a3103b3e9abfdaded91bb97770ab69963f2
kup <konka.um@gmail.com>
2023-10-19 20:48:47 +0700

    1 and 3 done

---
+++ lab1-2/main.py
@@ -0,0 +1,14 @@
+#1
+
+"""
+Name: Konkanok Umnartyuttithum
+Student number: 6642003026
+Address:    59/1 Charat Mueng Road,
+            Rong Mueng Sub-district,
+            Pathumwan District,
+            Bangkok 10330
+"""
+
+#3
+
+print("Hello, Konkanok Umnartyuttithum, 6642003026")
