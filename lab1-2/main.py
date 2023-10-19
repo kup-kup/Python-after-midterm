a02c7fb8812d27d56ac0f63a2c2f845ab9a7e1ad
kup <konka.um@gmail.com>
2023-10-19 21:03:30 +0700

    done to 7-c, added instuction file

--- lab1-2/main.py
+++ lab1-2/main.py
@@ -19,4 +19,32 @@ Address:    59/1 Charat Mueng Road,
 
 #5-b
 
-print(1, 2, 3, 4, sep = ":", end = "-")
+# print(1, 2, 3, 4, sep = ":", end = "-")
+
+#7-a
+
+# counter = 100
+# miles = 1000.00
+# name = "john"
+
+# print(counter, type(counter))
+# print(miles, type(miles))
+# print(name, type(name))
+
+#7-b
+
+# a, b, c = 1, 2, "john"
+
+# print(a)
+# print(b)
+# print(c)
+
+#7-c
+
+# a = b = c = 1
+
+# print(a)
+# print(b)
+# print(c)
+
+#8
