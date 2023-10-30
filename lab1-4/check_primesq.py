178aa22ea293dbcecebbba4ac6235c45c3fdb726
kup <konka.um@gmail.com>
2023-10-30 16:24:15 +0700

    prime number list done

---
+++ lab1-4/check_primesq.py
@@ -0,0 +1,16 @@
+end = int(input("Enter Your Number: "))
+num = 1
+
+while (num <= end):
+    qc = 2
+    prime_var = True
+    if (num == 1): prime_var = False
+
+    while (qc < num):
+        if (num % qc == 0): 
+            prime_var = False
+            break
+        qc += 1
+
+    print(f"{num: >{len(str(end))}}: {prime_var}")
+    num += 1
