6095a89fa7ef606c4ee05a102168720159c6cb05
kup <konka.um@gmail.com>
2023-11-13 14:24:41 +0700

    done to 2.3

--- lab4/main1.py
+++ lab4/main1.py
@@ -1,6 +1,43 @@
+# 1
+
 # def printme(str):
 #     print(str)
 #     return
 
 # printme("This is first call")
 # printme("Again second")
+
+# 2.1
+
+# def changeme( mylist ):
+#    print ("Values inside the function: ", mylist)
+   
+#    mylist[2]=50
+#    print ("Values inside the function after change: ", mylist)
+#    return
+
+# mylist = [10,20,30]
+# changeme( mylist )
+# print ("Values outside the function: ", mylist)
+
+# 2.2
+
+# def changeme( mylist ):
+#    mylist = [1,2,3,4] 
+#    print ("Values inside the function: ", mylist)
+#    return
+
+# mylist = [10,20,30]
+# changeme( mylist )
+# print ("Values outside the function: ", mylist)
+
+# 2.3
+
+# def changeme( myvar ):
+#    myvar = 100
+#    print ("Values inside the function: ", myvar)
+#    return
+
+# myvar = 99
+# changeme( myvar )
+# print ("Values outside the function: ", myvar)
